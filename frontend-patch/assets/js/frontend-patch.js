419e7386a0c2e032720d1d742baba6545b4a0e8c
Buria <52757103+BuriaBuria@users.noreply.github.com>
2022-12-06 17:46:22 +0200

    Update frontend-patch.js

--- frontend-patch/assets/js/frontend-patch.js
+++ frontend-patch/assets/js/frontend-patch.js
@@ -4,12 +4,12 @@ watchTrigger.observe(trigger, {childList:true});
 
 function displayPatch() {
     let target = document.querySelector('.abc-column');
-    let targetContent =target.innerHTML;
-    let end= targetContent.search('<input');
-    let start = targetContent.search('room:')+11;
+    let targetContent = target.innerHTML;
+    let end = targetContent.search('<input');
+    let start = targetContent.search('room:') + 11;
     if( -1 !== start && -1 !== end ) {
         let price = targetContent.substring(start, end).trim();
-        target.insertAdjacentHTML('beforeend', '<br><b>Price with discount:</b> ' + Math.round(price*0.7 ) );
+        target.insertAdjacentHTML('beforeend', '<br><b>Price with discount:</b> ' + Math.round( price * 0.7 ) );
     }
 }
 
